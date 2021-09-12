5d1a4a3f926b89d6cf09f92ee8976bcc8a976c43
phucbm <funnykingkong@gmail.com>
2021-09-12 11:18:29 +0700

    add rock paper scisscors game.

---
+++ freecodecamp_projects/rock_paper_scissors.py
@@ -0,0 +1,25 @@
+import random
+
+if __name__ == "__main__":
+    while True:
+        user = input("What is your choices? r:rock p:paper s:scissors   :")
+        computer = random.choice(["r","p","s"])
+        try:
+            (["r","p","s"].index(user))
+        except ValueError: 
+            print("Unknown!!! Try again!!!")
+            continue
+        
+        if (user == computer):
+            print("It's a tie! One more time!")
+        elif ((user == "r" and computer == "s") |
+            (user == "p" and computer == "r") |
+            (user == "s" and computer == "p")):
+            print("You WIN !!!")
+            break
+        else:
+            print("You LOSE !!! Try again?")
+        
+        
+        
+        
